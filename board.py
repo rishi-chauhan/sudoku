2c14d1e5d0ebbafef5bd882982fe478dc2532724
rishi-chauhan <rishi.darkknight.chauhan@gmail.com>
2021-10-23 16:54:25 +0530

    - Updates gitignore file to ingnore pychache folder - Completes board.py    - Adds class' and methods' description    - Completes print_board, get_row_board and get_col_board

--- board.py
+++ board.py
@@ -1,43 +1,47 @@
-"""This class is responsible for handling board of a sudoku game"""
+"""This module is responsible for handling the board"""
+from math import sqrt
+
 class Board:
+    """This class contains methods that help in manipulating the game board"""
     def __init__(self, entries):
-    """creates the board for the game"""
-        self.board = []
-        row=[]
-        for entry in entries:
-            row.append(entry)
-            if len(row)==9:
-                self.board.append(row)
-                row=[]
+        """initialises the board for the game"""
+        self.board = entries
+        self.dimension=int(sqrt(len(entries)))
 
     def print_board(self):
-    """Prints board in the current situation in 3x3 matrix"""
-        for index, row in enumerate(board):
-            if index % 3 and index != 0:
-                print(row, sep=' ')
+        """Prints board in the current state"""
+        print("+" + "---+"*self.dimension)
+        for ind, ele in enumerate(self.board):
+            if (ind+1)%self.dimension==0:
+                print(f"| {ele} |")
+                print("+" + "---+"*self.dimension)
             else:
-                print(row, sep=' ', end='\n')
+                print(f"| {ele} ", end="")
 
-    def row_board(entries):
+    def get_row_board(self):
+        """Returns sudoku board considering rows as major"""
         board = []
         row = []
-        for index, val in enumerate(entries):
+        for val in self.board:
             row.append(val)
-            if len(index) == 9:
+            if len(row) == self.dimension:
                 board.append(row)
                 row = []
         return board
 
-    def col_board(entries):
-        board = [[] for i in range(9)]
-        for index, val in enumerate(entries):
-            board[index % 9].append(val)
+    def get_col_board(self):
+        """Returns sudoku board considering columns as major"""
+        board = [[] for i in range(self.dimension)]
+        for index, val in enumerate(self.board):
+            board[index % self.dimension].append(val)
         return board
 
-    def box_board(entries):
-        board = []
-        box = []
-        for row in range(9):
-            for col in range(3):
-                box.append(entries[row])
-            pass
+    def get_box_board(self):
+        """Returns sudoku board considering box as major"""
+        board = [[] for i in range(self.dimension)]
+        box_dimension=int(sqrt(self.dimension))
+        for ind, ele in enumerate(self.board):
+            row=int(ind/self.dimension)
+            col=int(ind%self.dimension)
+            board[(box_dimension*int(row/box_dimension))+int(col/box_dimension)].append(ele)
+        return board
