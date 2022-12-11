e02ef5cf107fc5237e6a650399584ded72d1c67d
rishi-chauhan <rishi.darkknight.chauhan@gmail.com>
2022-12-11 21:08:24 +0530

    - Updates gitignore - Refactors board module by removing board class

--- board.py
+++ board.py
@@ -2,56 +2,51 @@
 from math import sqrt
 from typing import List
 
-class Board:
-    """This class contains methods that help in manipulating the game board"""
-    def __init__(self, entries: List[int]) -> None:
-        """initialises the board for the game"""
-        self.__board = entries
-        self.__dimension=int(sqrt(len(entries)))
+CORRECT_PATTERN = set(range(1,10))
+DIMENSION = 9
 
-    def get_board_dimension(self) -> int:
-        """getter function for board dimension"""
-        return self.__dimension
+def print_board(board) -> None:
+    """Prints board in the current state"""
+    print("+" + "---+"*DIMENSION)
+    for ind, ele in enumerate(board):
+        if (ind+1)%DIMENSION==0:
+            print(f"| {ele} |")
+            print("+" + "---+"*DIMENSION)
+        else:
+            print(f"| {ele} ", end="")
 
-    def get_board(self) -> List[int]:
-        """getter for sudoku board"""
-        return self.__board
+def get_row_board(board) -> List[List[int]]:
+    """Returns sudoku board considering rows as major"""
+    row_board = []
+    row = []
+    for val in board:
+        row.append(val)
+        if len(row) == DIMENSION:
+            row_board.append(row)
+            row = []
+    return board
 
-    def print_board(self) -> None:
-        """Prints board in the current state"""
-        dimension = self.get_board_dimension()
-        print("+" + "---+"*dimension)
-        for ind, ele in enumerate(self.get_board()):
-            if (ind+1)%dimension==0:
-                print(f"| {ele} |")
-                print("+" + "---+"*dimension)
-            else:
-                print(f"| {ele} ", end="")
+def get_col_board(board) -> List[List[int]]:
+    """Returns sudoku board considering columns as major"""
+    board = [[] for i in range(DIMENSION)]
+    for index, val in enumerate(board):
+        board[index % DIMENSION].append(val)
+    return board
 
-    def get_row_board(self) -> List[List[int]]:
-        """Returns sudoku board considering rows as major"""
-        board = []
-        row = []
-        for val in self.get_board():
-            row.append(val)
-            if len(row) == self.get_board_dimension():
-                board.append(row)
-                row = []
-        return board
+def get_box_board(board) -> List[List[int]]:
+    """Returns sudoku board considering box as major"""
+    board = [[] for i in range(DIMENSION)]
+    box_dimension=int(sqrt(DIMENSION))
+    for ind, ele in enumerate(board):
+        row=int(ind/DIMENSION)
+        col=int(ind%DIMENSION)
+        board[(box_dimension*int(row/box_dimension))+int(col/box_dimension)].append(ele)
+    return board
 
-    def get_col_board(self) -> List[List[int]]:
-        """Returns sudoku board considering columns as major"""
-        board = [[] for i in range(self.get_board_dimension())]
-        for index, val in enumerate(self.get_board()):
-            board[index % self.get_board_dimension()].append(val)
-        return board
-
-    def get_box_board(self) -> List[List[int]]:
-        """Returns sudoku board considering box as major"""
-        board = [[] for i in range(self.get_board_dimension())]
-        box_dimension=int(sqrt(self.get_board_dimension()))
-        for ind, ele in enumerate(self.get_board()):
-            row=int(ind/self.get_board_dimension())
-            col=int(ind%self.get_board_dimension())
-            board[(box_dimension*int(row/box_dimension))+int(col/box_dimension)].append(ele)
-        return board
+def check_if_correct(board):
+    """checks if array of elements is solved"""
+    for ele in board:
+        ele.sort()
+        if ele == CORRECT_PATTERN:
+            return True
+    return False
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 """Main class for sudoku game. Run this to solve the game."""
-from board import Board
+import board
 
 # ENTRIES contains the values of each cell
-ENTRIES = [0, 0, 0, 2, 6, 0, 7, 0, 1, 6, 8, 0, 0, 7, 0, 0, 9, 0, 1,
+BOARD = [9, 3, 4, 2, 6, 5, 7, 8, 1, 6, 8, 0, 0, 7, 0, 0, 9, 0, 1,
            9, 0, 0, 0, 4, 5, 0, 0, 8, 2, 0, 1, 0, 0, 0, 4, 0, 0,
            0, 4, 6, 0, 2, 9, 0, 0, 0, 5, 0, 0, 0, 3, 0, 2, 8, 0,
            0, 9, 3, 0, 0, 0, 7, 4, 0, 4, 0, 0, 5, 0, 0, 3, 6, 7,
            0, 3, 0, 1, 8, 0, 0, 0]
 
-board = Board(ENTRIES)
